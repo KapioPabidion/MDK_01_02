6e00b00de0ed016419b3687b1bf6c7cd7daeaccd
Anon1m4k <kirilvalera2@gmail.com>
2024-10-05 13:06:35 +0300

    05.10.2004

--- ClassLibrary1/Distances.cs
+++ ClassLibrary1/Distances.cs
@@ -39,26 +39,36 @@ namespace GeneratorDistanceOfTarget
         {
             angle = angle * 3.14 / 180;
 
-            double time = 0.0;
-            double deltaTime = 0.1;
-            double maxTime = 90.0; 
-
-            int numSteps = (int)(maxTime / deltaTime);
-            double[] x = new double[numSteps];
-            double[] y = new double[numSteps];
-
             /// REVIEW. a.boikov. 05/10/2024. Этот метод должен вычислить всего лишь координату падения.
             /// Она состоит из значения X и значения Y.
             /// Цикл не нужен. Обычная формула расчёта координаты падения
+
+            double distance = Math.Floor((Math.Pow(shell, 2) * Math.Sin(2 * angle)) / gravity + 0.5);
+            double x = distance * Math.Cos(angle);
+            double y = distance * Math.Sin(angle);
+
+            return distance; // return the distance
+        }
+
+        public (double, double) CalculateTrajectory(double angle)
+        {
+            angle = angle * 3.14 / 180;
+
+            double deltaTime = 0.1;
+            double maxTime = 90.0;
+            int numSteps = (int)(maxTime / deltaTime);
+
+            double[] x = new double[numSteps];
+            double[] y = new double[numSteps];
+
             for (int i = 0; i < numSteps; i++)
             {
-                time = i * deltaTime;
-                double distance = Math.Floor((Math.Pow(shell, 2) * Math.Sin(2 * angle)) / gravity + 0.5);
+                double time = i * deltaTime;
                 x[i] = distance * Math.Cos(angle);
                 y[i] = distance * Math.Sin(angle) - 0.5 * gravity * time * time;
             }
 
-            return time;
+            return (x[numSteps - 1], y[numSteps - 1]); // return the final x and y coordinates
         }
     }
 }
